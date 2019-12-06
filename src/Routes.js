db0d130cf209537d08be04fc19e74cda8ffd961b
Piyakulpinyo <Thanarath.Piyakulpinyo@adastragrp.com>
2019-12-06 16:20:36 +0700

    Setting up our React app

---
+++ src/Routes.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import NotFound from "./containers/NotFound";
+import Home from "./containers/Home";
+export default function Routes() {
+    return (
+        <Switch>
+            <Route path="/" exact component={Home} />
+            <Route component={NotFound} />
+        </Switch>
+    );
+}
---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+        REGION: "us-east-1",
+        BUCKET: "notes-app-ong"
+    },
+    apiGateway: {
+        REGION: "us-east-1",
+        URL: "https://ouzhfru8b3.execute-api.us-east-1.amazonaws.com/prod"
+    },
+    cognito: {
+        REGION: "us-east-1",
+        USER_POOL_ID: "us-east-1_TWEl5S07c",
+        APP_CLIENT_ID: "35um5tdu5v98bj9hip9tfcurrm",
+        IDENTITY_POOL_ID: "35um5tdu5v98bj9hip9tfcurrm"
+    }
+};
