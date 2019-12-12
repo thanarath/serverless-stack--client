a7932f598a3e1bf67842e5b6d25c9527bfd0fbb8
Piyakulpinyo <Thanarath.Piyakulpinyo@adastragrp.com>
2019-12-12 10:57:32 +0700

    Setting up our React app

--- src/Routes.js
+++ src/Routes.js
@@ -2,10 +2,19 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import NotFound from "./containers/NotFound";
 import Home from "./containers/Home";
-export default function Routes() {
+import Login from "./containers/Login";
+import Signup from "./containers/Signup";
+import AppliedRoute from "./components/AppliedRoute";
+import NewNote from "./containers/NewNote";
+
+export default function Routes({ appProps }) {
     return (
         <Switch>
-            <Route path="/" exact component={Home} />
+            <AppliedRoute path="/" exact component={Home} appProps={appProps} />
+            <AppliedRoute path="/login" exact component={Login} appProps={appProps} />
+            <AppliedRoute path="/signup" exact component={Signup} appProps={appProps} />
+            <AppliedRoute path="/notes/new" exact component={NewNote} appProps={appProps} />
+            { /* Finally, catch all unmatched routes */}
             <Route component={NotFound} />
         </Switch>
     );
--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 export default {
+    MAX_ATTACHMENT_SIZE: 5000000,
     s3: {
         REGION: "us-east-1",
         BUCKET: "notes-app-ong"
@@ -11,6 +12,6 @@ export default {
         REGION: "us-east-1",
         USER_POOL_ID: "us-east-1_TWEl5S07c",
         APP_CLIENT_ID: "35um5tdu5v98bj9hip9tfcurrm",
-        IDENTITY_POOL_ID: "35um5tdu5v98bj9hip9tfcurrm"
+        IDENTITY_POOL_ID: "us-east-1:17d7e4db-43d6-4784-9243-73318a1a1a65"
     }
 };
